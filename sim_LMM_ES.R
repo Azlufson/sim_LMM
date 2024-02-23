1392418954be37e0e3cbe5f7507fed1e17e695be
Azlufson <hermann.stigler@gmx.at>
2024-02-23 17:12:35 +0100

    copied sim_LMM_n

--- sim_LMM_ES.R
+++ sim_LMM_ES.R
@@ -1,4 +1,23 @@
-##Stärke des Effekts
+#Alpha-Fehler, Güte:
+#Modell erstellen
+#testen: fixed effects, random effects
+#Parameter: Stichprobengröße, Stärke des Effekts, Komplexität des Modells, missing values, balanciertes Design
+#Schätzmethoden:  ML, REML
+#                 Sattherwaite, Kenward-Rogers
+#                 MCMC (Baayen et al., 2008)
+#                 t as z
+
+#mcmc nicht mehr unterstützt von lme4
+#library(languageR)
+#lmm <- lmer(model, sim_data.n_int())
+#pvals.fnc(lmm)
+
+####Effektstärke
+
+
+##TODO: KR für REML?, PB für REML?
+##      funktionen aus anderem skript importieren?
+##      n.subj, n.obs für ungerade zahlen
 
 library(future.apply)
 library(parallel)
@@ -8,7 +27,11 @@ library(lmerTest)
 library(pbkrtest)
 library(afex)
 
-sim_data.n_int <- function(n.subj, n.obs, b0, beta_obs, beta_cond, sd.int_subj, sd_eps) {
+##Datengeneration
+#einfaches Modell nur mit random intercept
+# y = b0 + b1*obs + b2*cond + (1|subj) + epsilon
+#n.subj und n.obs müssen gerade sein
+sim_data.n_int <- function(n.subj = 10, n.obs = 6, b0 = 10, beta_obs = 0, beta_cond = 5, sd.int_subj = 6, sd_eps = 2) {
   subj <- rep(1:n.subj, each = n.obs)
   obs <- rep(rep(c(0,1), each = n.obs/2), n.subj)
   cond <- rep(c(0,1), n.subj*n.obs/2)
@@ -73,4 +96,249 @@ plan("multisession", workers = detectCores())
 
 #Parameter für parametric bootstrap
 nsim.mixed <- 100 #niedriger, weil pro iteration auch noch gebootstrapped wird (mit nsim.pb)
-nsim.pb <- 500 
+nsim.pb <- 500
+
+###LRT
+##REML (nicht empfohlen)
+data_LRT.REML <- t(future_apply(grid, 1, function(x) replicate(nsim, test_lrtstat(sim_data.n_int(n.subj = x[1], n.obs = x[2], beta_obs = beta_obs), m.full, m.null)), future.seed = TRUE))
+data_LRT.REML_long <- cbind(grid, data_LRT.REML)
+data_LRT.REML_long <- gather(data_LRT.REML_long, sim, p.LRT.REML, (ncol(grid)+1):ncol(data_LRT.REML_long))
+
+data_LRT.REML_long %>% 
+  group_by(n.subj, n.obs) %>% 
+  summarize(prop_LRT.REML = mean(p.LRT.REML <= .05))
+
+##Daten für Plot:
+p_LRT.REML <- data_LRT.REML_long %>% 
+  group_by(n.subj, n.obs) %>% 
+  summarize(k = sum(p.LRT.REML < .05) + 1.96^2/2,
+            n = n() + 1.96^2,
+            p = k/n,
+            p_l = p - 1.96 * sqrt(p*(1-p)/n),
+            p_u = p + 1.96 * sqrt(p*(1-p)/n)) %>% 
+  select(n.obs, n.subj, p, p_l, p_u) %>% 
+  mutate(REML = 1,
+         method = 1)
+
+##ML
+data_LRT.ML <- t(future_apply(grid, 1, function(x) replicate(nsim, test_lrtstat(sim_data.n_int(n.subj = x[1], n.obs = x[2], beta_obs = beta_obs), m.full, m.null, REML = FALSE)), future.seed = TRUE))
+data_LRT.ML_long <- cbind(grid, data_LRT.ML)
+data_LRT.ML_long <- gather(data_LRT.ML_long, sim, p.LRT.ML, (ncol(grid)+1):ncol(data_LRT.ML_long))
+
+data_LRT.ML_long %>% 
+  group_by(n.subj, n.obs) %>% 
+  summarize(prop_LRT.ML = mean(p.LRT.ML <= .05))
+
+p_LRT.ML <- data_LRT.ML_long %>% 
+  group_by(n.subj, n.obs) %>% 
+  summarize(k = sum(p.LRT.ML < .05) + 1.96^2/2,
+            n = n() + 1.96^2,
+            p = k/n,
+            p_l = p - 1.96 * sqrt(p*(1-p)/n),
+            p_u = p + 1.96 * sqrt(p*(1-p)/n)) %>% 
+  select(n.obs, n.subj, p, p_l, p_u) %>% 
+  mutate(REML = 0,
+         method = 1)
+
+###t-as-z
+##REML
+data_TasZ.REML <- t(future_apply(grid, 1, function(x) replicate(nsim, test_TasZ.fixed(sim_data.n_int(n.subj = x[1], n.obs = x[2], beta_obs = beta_obs), model)), future.seed = TRUE))
+data_TasZ.REML_long <- cbind(grid, data_TasZ.REML)
+data_TasZ.REML_long <- gather(data_TasZ.REML_long, sim, p.TasZ.REML, (ncol(grid)+1):ncol(data_TasZ.REML_long))
+
+data_TasZ.REML_long %>% 
+  group_by(n.subj, n.obs) %>% 
+  summarize(prop_TasZ.REML = mean(abs(p.TasZ.REML) >= 1.96))
+
+p_TasZ.REML <- data_TasZ.REML_long %>% 
+  group_by(n.subj, n.obs) %>% 
+  summarize(k = sum(abs(p.TasZ.REML) >= 1.96) + 1.96^2/2,
+            n = n() + 1.96^2,
+            p = k/n,
+            p_l = p - 1.96 * sqrt(p*(1-p)/n),
+            p_u = p + 1.96 * sqrt(p*(1-p)/n)) %>% 
+  select(n.obs, n.subj, p, p_l, p_u) %>% 
+  mutate(REML = 1,
+         method = 2)
+
+##ML
+data_TasZ.ML <- t(future_apply(grid, 1, function(x) replicate(nsim, test_TasZ.fixed(sim_data.n_int(n.subj = x[1], n.obs = x[2], beta_obs = beta_obs), model, REML = FALSE)), future.seed = TRUE))
+data_TasZ.ML_long <- cbind(grid, data_TasZ.ML)
+data_TasZ.ML_long <- gather(data_TasZ.ML_long, sim, p.TasZ.ML, (ncol(grid)+1):ncol(data_TasZ.ML_long))
+
+data_TasZ.ML_long %>% 
+  group_by(n.subj, n.obs) %>% 
+  summarize(prop_TasZ.ML = mean(abs(p.TasZ.ML) >= 1.96))
+
+p_TasZ.ML <- data_TasZ.ML_long %>% 
+  group_by(n.subj, n.obs) %>% 
+  summarize(k = sum(abs(p.TasZ.ML) >= 1.96) + 1.96^2/2,
+            n = n() + 1.96^2,
+            p = k/n,
+            p_l = p - 1.96 * sqrt(p*(1-p)/n),
+            p_u = p + 1.96 * sqrt(p*(1-p)/n)) %>% 
+  select(n.obs, n.subj, p, p_l, p_u) %>% 
+  mutate(REML = 0,
+         method = 2)
+
+###KR, SW
+#anova aus lmertest
+
+##Sattherthwaire, REML
+ddf <- "Satterthwaite"
+REML <- TRUE
+data_SW.REML <- t(future_apply(grid, 1, function(x) replicate(nsim, test_approx.fixed(sim_data.n_int(n.subj = x[1], n.obs = x[2], beta_obs = beta_obs), model, REML = REML, ddf = ddf)), future.seed = TRUE))
+data_SW.REML_long <- cbind(grid, data_SW.REML)
+data_SW.REML_long <- gather(data_SW.REML_long, sim, p.SW.REML, (ncol(grid)+1):ncol(data_SW.REML_long))
+
+data_SW.REML_long %>% 
+  group_by(n.subj, n.obs) %>% 
+  summarize(prop_SW.REML = mean(p.SW.REML <= .05))
+
+p_SW.REML <- data_SW.REML_long %>% 
+  group_by(n.subj, n.obs) %>% 
+  summarize(k = sum(p.SW.REML < .05) + 1.96^2/2,
+            n = n() + 1.96^2,
+            p = k/n,
+            p_l = p - 1.96 * sqrt(p*(1-p)/n),
+            p_u = p + 1.96 * sqrt(p*(1-p)/n)) %>% 
+  select(n.obs, n.subj, p, p_l, p_u) %>% 
+  mutate(REML = 1,
+         method = 3)
+
+##Kenward-Roger, REML
+ddf <- "Kenward-Roger"
+REML <- TRUE
+data_KR.REML <- t(future_apply(grid, 1, function(x) replicate(nsim, test_approx.fixed(sim_data.n_int(n.subj = x[1], n.obs = x[2], beta_obs = beta_obs), model, REML = TRUE, ddf = "Satterthwaite")), future.seed = TRUE))
+data_KR.REML_long <- cbind(grid, data_KR.REML)
+data_KR.REML_long <- gather(data_KR.REML_long, sim, p.KR.REML, (ncol(grid)+1):ncol(data_KR.REML_long))
+
+data_KR.REML_long %>% 
+  group_by(n.subj, n.obs) %>% 
+  summarize(prop_KR.REML = mean(p.KR.REML <= .05))
+
+p_KR.REML <- data_KR.REML_long %>% 
+  group_by(n.subj, n.obs) %>% 
+  summarize(k = sum(p.KR.REML < .05) + 1.96^2/2,
+            n = n() + 1.96^2,
+            p = k/n,
+            p_l = p - 1.96 * sqrt(p*(1-p)/n),
+            p_u = p + 1.96 * sqrt(p*(1-p)/n)) %>% 
+  select(n.obs, n.subj, p, p_l, p_u) %>% 
+  mutate(REML = 1,
+         method = 4)
+
+##Sattherthwaire, ML
+ddf <- "Satterthwaite"
+REML <- FALSE
+data_SW.ML <- t(future_apply(grid, 1, function(x) replicate(nsim, test_approx.fixed(sim_data.n_int(n.subj = x[1], n.obs = x[2], beta_obs = beta_obs), model, REML = TRUE, ddf = "Satterthwaite")), future.seed = TRUE))
+data_SW.ML_long <- cbind(grid, data_SW.ML)
+data_SW.ML_long <- gather(data_SW.ML_long, sim, p.SW.ML, (ncol(grid)+1):ncol(data_SW.ML_long))
+
+data_SW.ML_long %>% 
+  group_by(n.subj, n.obs) %>% 
+  summarize(prop_SW.ML = mean(p.SW.ML <= .05))
+
+p_SW.ML <- data_SW.ML_long %>% 
+  group_by(n.subj, n.obs) %>% 
+  summarize(k = sum(p.SW.ML < .05) + 1.96^2/2,
+            n = n() + 1.96^2,
+            p = k/n,
+            p_l = p - 1.96 * sqrt(p*(1-p)/n),
+            p_u = p + 1.96 * sqrt(p*(1-p)/n)) %>% 
+  select(n.obs, n.subj, p, p_l, p_u) %>% 
+  mutate(REML = 0,
+         method = 3)
+
+#Kenward-Roger nur für ML möglich!
+
+
+###parametric bootstrap (nur ML)
+
+#Cluster festlegen (future_apply funktioniert nicht)
+(nc <- detectCores()) # number of cores
+cl <- makeCluster(rep("localhost", nc)) # make cluster
+
+data_alpha.nB <- t(apply(grid, 1, function(x) replicate(nsim.mixed, test_PB.fixed(model, data = sim_data.n_int(n.subj = x[1], n.obs = x[2], beta_obs = beta_obs), nsim.pb = nsim.pb, cl = cl))))
+data_alpha.nB_long <- cbind(grid, data_alpha.nB)
+data_alpha.nB_long <- gather(data_alpha.nB_long, sim, p.PB, (ncol(grid)+1):ncol(data_alpha.nB_long))
+
+data_alpha.nB_long %>% 
+  group_by(n.subj, n.obs) %>% 
+  summarize(prop_PB = mean(p.PB <= .05))
+
+p_PB <- data_alpha.nB_long %>% 
+  group_by(n.subj, n.obs) %>% 
+  summarize(k = sum(p.PB < .05) + 1.96^2/2,
+            n = n() + 1.96^2,
+            p = k/n,
+            p_l = p - 1.96 * sqrt(p*(1-p)/n),
+            p_u = p + 1.96 * sqrt(p*(1-p)/n)) %>% 
+  select(n.obs, n.subj, p, p_l, p_u) %>% 
+  mutate(REML = 0,
+         method = 5)
+
+### Grafiken der Ergebnisse
+data_alpha.n <- rbind(p_TasZ.ML, p_TasZ.REML, p_LRT.ML, p_LRT.REML, p_SW.ML, p_SW.REML, p_KR.REML, p_PB)
+data_alpha.n$n.obs <- as.factor(data_alpha.n$n.obs)
+data_alpha.n$n.subj <- as.factor(data_alpha.n$n.subj)
+data_alpha.n$REML <- factor(data_alpha.n$REML, labels = c("ML", "REML"))
+data_alpha.n$method <- factor(data_alpha.n$method, labels = c("LRT", "t-as-z", "Satterthwaite", "Kenward-Roger", "Parametric Bootstrap"))
+
+#alle Methoden
+ggplot(data_alpha.n, aes(x = as.factor(n.obs), y = p, col = REML, shape = method)) + 
+  geom_point(position = position_dodge(.6)) + 
+  geom_errorbar(aes(ymin = p_l, ymax = p_u), position = position_dodge(.6), width = .3) +
+  geom_hline(yintercept = .05) +
+  facet_wrap(~n.subj) +
+  ylim(0, .12)
+
+#nur SW und KR
+data_alpha.n %>% 
+  filter(method %in% c("Satterthwaite", "Kenward-Roger")) %>% 
+  ggplot(aes(x = as.factor(n.obs), y = p, col = REML, shape = method)) + 
+  geom_point(position = position_dodge(.6)) + 
+  geom_errorbar(aes(ymin = p_l, ymax = p_u), position = position_dodge(.6), width = .3) +
+  geom_hline(yintercept = .05) +
+  facet_wrap(~n.subj, nrow = 1) +
+  ylim(0, .1)
+
+#nur SW
+data_alpha.n %>% 
+  filter(method %in% c("Satterthwaite")) %>% 
+  ggplot(aes(x = as.factor(n.obs), y = p, col = REML, shape = method)) + 
+  geom_point(position = position_dodge(.6)) + 
+  geom_errorbar(aes(ymin = p_l, ymax = p_u), position = position_dodge(.6), width = .3) +
+  geom_hline(yintercept = .05) +
+  facet_wrap(~n.subj, nrow = 1) +
+  ylim(0, .1)
+
+#nur ML
+data_alpha.n %>% 
+  filter(REML == "ML") %>% 
+  ggplot(aes(x = as.factor(n.obs), y = p, col = method)) + 
+  geom_point(position = position_dodge(.6)) + 
+  geom_errorbar(aes(ymin = p_l, ymax = p_u), position = position_dodge(.6), width = .3) +
+  geom_hline(yintercept = .05) +
+  facet_wrap(~n.subj, nrow = 1) +
+  ylim(0, .12)
+
+#nur REML
+data_alpha.n %>% 
+  filter(REML == "REML") %>% 
+  ggplot(aes(x = as.factor(n.obs), y = p, col = method)) + 
+  geom_point(position = position_dodge(.6)) + 
+  geom_errorbar(aes(ymin = p_l, ymax = p_u), position = position_dodge(.6), width = .3) +
+  geom_hline(yintercept = .05) +
+  facet_wrap(~n.subj, nrow = 1) +
+  ylim(0, .1)
+
+#nur t as z
+data_alpha.n %>% 
+  filter(method == "t-as-z") %>% 
+  ggplot(aes(x = as.factor(n.obs), y = p, col = REML)) + 
+  geom_point(position = position_dodge(.6)) + 
+  geom_errorbar(aes(ymin = p_l, ymax = p_u), position = position_dodge(.6), width = .3) +
+  geom_hline(yintercept = .05) +
+  facet_wrap(~n.subj, nrow = 1) +
+  ylim(0, .12)
